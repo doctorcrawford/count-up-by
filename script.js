844b4d1e630ed2b2f1aeb8cd32eeb78fb51c7acd
Kyle Crawford <kcraw217@gmail.com>
2023-03-29 14:43:18 -0700

    add showResults UI logic and add if statement for NaN
    co-authored-by: raed alkhanbashi <raed.alkhanbashi@gmail.com>

--- script.js
+++ script.js
@@ -12,7 +12,26 @@ function countUpBy(numberTo, numberBy) {
 
 window.onload = function() {
   let form = document.querySelector("form");
-  const firstNumInput = document.getElementById("firstNumInput");
-  const secondNumInput = document.getElementById("secondNumInput");
+
+
+  function numbers(e) {
+    e.preventDefault();
+    const firstNumInput = parseInt(document.getElementById("firstNumInput").value);
+    const secondNumInput = parseInt(document.getElementById("secondNumInput").value);
+
+    if(isNaN(firstNumInput) || isNaN(secondNumInput)) {
+      showResults('Please enter only integers');
+    } else {
+      const output = countUpBy(firstNumInput, secondNumInput);
+      showResults(output);
+    }
+  }
+  form.addEventListener("submit", numbers);
+
+  function showResults(result) {
+    let p = document.querySelector('p');
+
+    p.innerText = result;
+  }
 }
 
